f889fb982dfe9f5dc2d4320467d18e8b5113f70c
zino <minzino@hanmail.net>
2020-03-10 11:44:47 +0900

    Initial Commit

---
+++ WebSocketClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebSocketClient
+//
+//  Created by zino-mac on 2020/03/10.
+//  Copyright © 2020 zino-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
